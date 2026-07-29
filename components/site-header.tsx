03c75efddd348112ea670c1c9d890f58f3605972
Daniel Hughes <43981251+DanHues@users.noreply.github.com>
2026-07-29 00:21:16 -0400

    Make subpage navigation non-floating

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,11 +1,11 @@
 import { ContactFooter } from "@/components/contact-footer";
 import { ScrollHeader } from "@/components/scroll-header";
 
-export function SiteHeader() {
+export function SiteHeader({ floating = false }: { floating?: boolean }) {
   const basePath = process.env.PAGES_BASE_PATH || "";
 
   return (
-    <ScrollHeader basePath={basePath} />
+    <ScrollHeader basePath={basePath} floating={floating} />
   );
 }
 
